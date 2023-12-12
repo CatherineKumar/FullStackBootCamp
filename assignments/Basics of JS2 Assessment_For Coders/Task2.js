d4c04f3a1ea7f206fdec7b8f6692013831530cc1
ckumar <catherine.a.kumar@gmail.com>
2023-12-11 22:29:18 -0600

    save files

---
+++ assignments/Basics of JS2 Assessment_For Coders/Task2.js
@@ -0,0 +1,45 @@
+// 1. Define 2 functions
+// 1st function named as checkEven which will check if the num passed is even or not.
+// 2nd function named as filterEvens which will take an array of numbers and the checkEven function as arguments.
+// This filterEvens function will filter out  only even numbers using the checkEven function and generate a new array of the even numbers.
+
+//COMPLETE YOUR CODE HERE 
+
+//2. Write an IIFE that calculates the factorial of a given number and immediately logs the result to the console.
+
+//COMPLETE YOUR CODE HERE 
+
+//3. Implement a function "calculate" that takes three arguments: a, b, and an operation function. 
+//The operation function should accept two parameters and perform a specific mathematical operation like addition, subtraction, multiplication and division. 
+//Use call(), apply(), or bind() to apply the operation function to the arguments a and b.
+
+//COMPLETE YOUR CODE HERE 
+
+//4. Given an array of person objects, define a function to find oldest person object.
+
+persons = [{"name" : "Harry", "age" : 12}, {"name" : "Ron", "age" : 11}, {"name" : "Hermione", "age" : 13}]
+//COMPLETE YOUR CODE HERE 
+
+//5.  Create a function that calculates the sum of an array using IIFE function.
+
+//COMPLETE YOUR CODE HERE 
+
+//6. Write a function printContext that, when invoked, logs the this keyword to the console. Then, demonstrate how the context of a function can change when calling it with different objects using the call method.
+
+//COMPLETE YOUR CODE HERE 
+
+//7. Create a function multiply that takes two parameters and returns their product. Use the bind method to create a new function "double" that multiplies a single parameter by 2.
+
+//COMPLETE YOUR CODE HERE 
+
+// 8. Create an object person with properties name and age. Write a function "introduce" that logs a message introducing the person. Then, use the call method to invoke the introduce function with the person object as the context.
+
+//COMPLETE YOUR CODE HERE 
+
+// 9. Write a higher order function createMultiplier that takes a factor as an argument and returns another functiom that multiplies a number by that factor. 
+
+//COMPLETE YOUR CODE HERE 
+
+// 10. Write a function called "calculate" that adds two numbers and assign a property "description" to it with a string describing what the function does. Then, access and log this property.
+
+//COMPLETE YOUR CODE HERE 
---
+++ assignments/JS1_assignment/test.js
@@ -0,0 +1,17 @@
+const sum = (function(x,y){
+
+    return x+y;
+
+ })(10,20);
+
+  
+
+const sumAgain = function(x,y){
+
+ 
+
+    return x+y;
+
+}
+
+console.log(sum == sumAgain)
