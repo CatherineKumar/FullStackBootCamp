1a496f6c0c111ba386d30454ca33a051cba4cb85
CatherineKumar <catherine.a.kumar@gmail.com>
2024-02-04 18:50:22 -0600

    save work

--- assignments/Data Structures using JavaScript Assessment_For Coders/queue.js
+++ assignments/Data Structures using JavaScript Assessment_For Coders/queue.js
@@ -1,34 +1,62 @@
 //Problem Statement 3
 class PrinterQueue {
   constructor() {
-    // Initialize an empty array to store documents in the queue
+      // Initialize an empty array to store documents in the queue
+      this.items = []
   }
 
   isEmpty() {
-    // Check if the printer queue is empty
+      // Check if the printer queue is empty
+      if ( this.items.length == 0 )
+      {
+        return true
+      }
+      else
+      {
+        return false
+      }     
   }
 
   // Add a new document to the printer queue
   enqueue(document) {
     // Push the specified document onto the end of the queue
     // Log a message indicating that the document has been added to the printer queue
+    this.items.push(document)
+    console.log('Document added to printer queue: ', document)
   }
 
   // Remove and return the first document from the printer queue
   dequeue() {
     // Check if the queue is empty - Log a message indicating that the printer queue is empty
     // Remove and return the first document from the queue
+    if ( this.isEmpty() )
+    {
+      console.log('Printer queue is empty.')
+    }
+    else
+    {
+      return this.items.shift()
+    }
   }
 
   // Retrieve the first document in the printer queue without removing it
   peek() {
     // Check if the queue is empty - Log a message indicating that the printer queue is empty
     // Return a message indicating the next document to print
+    if ( this.isEmpty() )
+    {
+      console.log('Printer queue is empty.')
+    }
+    else
+    {
+      return this.items[0]
+    }
   }
 
   // Display all documents in the printer queue
   displayQueue() {
     // Log each document in the queue on a new line
+    this.items.forEach((document) => console.log(document))
   }
 }
 
