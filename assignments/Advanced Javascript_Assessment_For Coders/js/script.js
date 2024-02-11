1b2f6e5460c91eaa6126b520552499bbf1619080
CatherineKumar <catherine.a.kumar@gmail.com>
2024-02-11 15:51:30 -0600

    save work

--- assignments/Advanced Javascript_Assessment_For Coders/js/script.js
+++ assignments/Advanced Javascript_Assessment_For Coders/js/script.js
@@ -33,6 +33,40 @@ function fetchRegisterPageData() {
 function signIn() {
   // Write your code for task2 here
   // Use fetchSignInPageData() function to fetch values
+  const { username, password, messageElement } = fetchSignInPageData()
+
+  try {
+      // Fetch userInfo from the local storage
+      const registeredUser = JSON.parse(localStorage.getItem("user"));
+      console.log(registeredUser);
+
+      // Check if the entered username or password is empty
+      if (username === "" || password === "") {
+          messageElement.textContent =
+              "Username and Password fields cannot be empty!!";
+      } else {
+          // Check if the username and password of the registeredUser from local storage matches the entered username and password
+          if (
+              username === registeredUser.username &&
+              password === registeredUser.password
+          ) {
+              // Set the session storage with the login info
+              sessionStorage.setItem("login", true);
+              window.location.href = "../html/index.html";
+          } else {
+
+              // If the username or password does not match, display error message
+              messageElement.textContent = "Incorrect Username or Password!!";
+              // Set the login info in the session storage as false
+              sessionStorage.setItem("login", false);
+          }
+      }
+  } catch (err) {
+      // If the user is not registered, display appropriate error message
+      messageElement.textContent =
+          "The user is not registered yet. Please click on the register button to register yourself!!";
+  }
+
   
 }
 
