a8693bb05cebaa8ba365c34c6f82313a2ae831c1
CatherineKumar <catherine.a.kumar@gmail.com>
2024-02-12 08:18:50 -0600

    save work

--- assignments/Advanced Javascript_Assessment_For Coders/js/script.js
+++ assignments/Advanced Javascript_Assessment_For Coders/js/script.js
@@ -82,70 +82,78 @@ function signIn()
 }
 
 // Function to fetch user data from the signin.html page
-function fetchSignInPageData() {
-    // Fetch data from the signin form 
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
-    const messageElement = document.getElementById("message");
+function fetchSignInPageData() 
+{
+      // Fetch data from the signin form 
+      const username = document.getElementById("username").value;
+      const password = document.getElementById("password").value;
+      const messageElement = document.getElementById("message");
 
-    return { username, password, messageElement };
+      return { username, password, messageElement };
 }
 
 // Function to load content on the index.html page
-function loadContent() {
-  // Fetch the login info from the session storage
-  const userLogin = sessionStorage.getItem("login");
+function loadContent() 
+{
+    // Fetch the login info from the session storage
+    const userLogin = sessionStorage.getItem("login");
 
-  // Check whether the login status is true
-  if (userLogin) {
-    fetchAndLoadData();
-  }
-  else {
-    // If login status is false, redirect to signin.html page
-    loadSignInPage();
-  }
-
+    // Check whether the login status is true
+    if (userLogin) 
+    {
+        fetchAndLoadData();
+    }
+    else 
+    {
+        // If login status is false, redirect to signin.html page
+        loadSignInPage();
+    }
 }
 
 // Fetch the URLs from the external API
-function fetchURLs() {
-  const cuisineUrl = "https://foodorder-api-elti.onrender.com/v1/cuisines";
-  const categoryUrl = "https://foodorder-api-elti.onrender.com/v1/categories";
-  const restaurantUrl = "https://foodorder-api-elti.onrender.com/v1/restaurants";
+function fetchURLs() 
+{
+    const cuisineUrl = "https://foodorder-api-elti.onrender.com/v1/cuisines";
+    const categoryUrl = "https://foodorder-api-elti.onrender.com/v1/categories";
+    const restaurantUrl = "https://foodorder-api-elti.onrender.com/v1/restaurants";
 
-  return { cuisineUrl, categoryUrl, restaurantUrl }
+    return { cuisineUrl, categoryUrl, restaurantUrl }
 }
 
 // Get the list HTML elements to display cuisines, categories and restaurants
-function loadListElements() {
-  const cuisineList = document.getElementById("cuisine-list");
-  const categoryList = document.getElementById("category-list");
-  const restaurantList = document.getElementById("restaurant-list");
+function loadListElements() 
+{
+    const cuisineList = document.getElementById("cuisine-list");
+    const categoryList = document.getElementById("category-list");
+    const restaurantList = document.getElementById("restaurant-list");
 
-  return { cuisineList, categoryList, restaurantList }
+    return { cuisineList, categoryList, restaurantList }
 }
 
 // Fetch the data from the URLs
-function fetchAndLoadData() {
-  const { cuisineUrl, categoryUrl, restaurantUrl } = fetchURLs()
+function fetchAndLoadData() 
+{
+    const { cuisineUrl, categoryUrl, restaurantUrl } = fetchURLs()
 
-  // Fetch the list elements from index.html which will store cuisines, categories and restaurants data
-  const { cuisineList, categoryList, restaurantList } = loadListElements()
+    // Fetch the list elements from index.html which will store cuisines, categories and restaurants data
+    const { cuisineList, categoryList, restaurantList } = loadListElements()
 
-  // Call the fetchData function to fetch the data from the above-mentioned endpoints
-  fetchData(cuisineUrl, cuisineList);
-  fetchData(categoryUrl, categoryList);
-  fetchData(restaurantUrl, restaurantList);
+    // Call the fetchData function to fetch the data from the above-mentioned endpoints
+    fetchData(cuisineUrl, cuisineList);
+    fetchData(categoryUrl, categoryList);
+    fetchData(restaurantUrl, restaurantList);
 }
 
 // Load the signin.html page
-function loadSignInPage() {
-  window.location.href = "../html/signin.html";
+function loadSignInPage() 
+{
+    window.location.href = "../html/signin.html";
 }
 
 
 // Function to fetch data from an external URL endpoint
-async function fetchData(url, listElement) {
+async function fetchData(url, listElement) 
+{
     // Write your code for task3 here
     try 
     {
@@ -166,7 +174,6 @@ async function fetchData(url, listElement) {
 function displayData(data, listElement) 
 {
     // Write your code for task4 here
-  
     data.data.forEach(item => {
       // Create HTML structure for displaying each of the item details
       const itemTable = `<div class="item-card">
@@ -174,16 +181,14 @@ function displayData(data, listElement)
           <img src="${item.image}" style="width:100%; height:20vh;">
           </div>`    
       listElement.innerHTML += itemTable;
-
-});  
-  
+      });  
   
 }
 
 // Logging out when clicked on the logout button
 function logout(){
-  // Write your code for task5 here
-  sessionStorage.setItem("login", false)
-  sessionStorage.setItem("activeUser","")
-  loadSignInPage()
+    // Write your code for task5 here
+    sessionStorage.setItem("login", false)
+    sessionStorage.setItem("activeUser","")
+    loadSignInPage()
 }
