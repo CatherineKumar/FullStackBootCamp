20cfa5ad8e4805c8265ea90fbbf811a028cd83d7
ckumar <catherine.a.kumar@gmail.com>
2024-03-04 20:31:19 -0600

    save order

--- assignments/React Week1 Assessment_For Coders/Food_Order_App/src/pages/userview/HomePage.js
+++ assignments/React Week1 Assessment_For Coders/Food_Order_App/src/pages/userview/HomePage.js
@@ -8,26 +8,49 @@ import restaurants from "../../restaurants";
 const HomePage = () => {
   return (
     <>
-      <div className="container-fluid">
+      <div className="container-fluid" style={{ marginBottom: '4rem' }}>
         {/* Implement Task 1 - Displaying cuisine names and images */}
-         <h4>Try New Cuisines</h4>
+        <h4 style={{ margin: '30px 0' }}> Try New Cuisines</h4>
          <Row>
          {cuisines.map(cuisine=> (
-              <Card style={{ width: '12rem' }} className="m-1">
+              <Card style={{ width: '12rem' }} className="m-2">
               <Card.Img variant="top" src={cuisine.image} style={{ width: '100%', height: '200px', objectFit: 'cover' }} />
               <Card.Body>
                 <Card.Title>{cuisine.name}</Card.Title>
-
               </Card.Body>
             </Card>
          ))}
          </Row>
       </div>
+
       <div className="container-fluid">
         {/* Implement Task 1 - Displaying category names and images */}
+        <h4 style={{ margin: '30px 0' }}>Get inspiration for your order</h4>
+        <Row>
+            {categories.map(category=> (
+                <Card style={{ width: '12rem' }} className="m-2">
+                <Card.Img variant="top" src={category.image} style={{ width: '100%', height: '200px', objectFit: 'cover' }} />
+                <Card.Body>
+                    <Card.Title>{category.name}</Card.Title>
+                </Card.Body>
+                </Card>
+            ))}
+        </Row>
       </div>
+
       <div className="container-fluid">
         {/* Implement Task 1 - Displaying restaurant names and images */}
+        <h4 style={{ margin: '30px 0' }}>Top Restaurants</h4>
+        <Row>
+            {restaurants.map(restaurant=> (
+                <Card style={{ width: '12rem' }} className="m-2">
+                <Card.Img variant="top" src={restaurant.image} style={{ width: '100%', height: '200px', objectFit: 'cover' }} />
+                <Card.Body>
+                    <Card.Title>{restaurant.name}</Card.Title>
+                </Card.Body>
+                </Card>
+            ))}
+        </Row>
       </div>
     </>
   );
