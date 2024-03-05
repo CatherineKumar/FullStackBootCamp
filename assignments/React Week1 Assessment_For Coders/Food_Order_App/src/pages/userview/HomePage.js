c61de2d448e89aa61e784ddecf3b7fdd95ef19e0
ckumar <catherine.a.kumar@gmail.com>
2024-03-04 19:50:29 -0600

    save work

--- assignments/React Week1 Assessment_For Coders/Food_Order_App/src/pages/userview/HomePage.js
+++ assignments/React Week1 Assessment_For Coders/Food_Order_App/src/pages/userview/HomePage.js
@@ -15,10 +15,9 @@ const HomePage = () => {
               <Card style={{ width: '18rem' }}>
               <Card.Img variant="top" src={cuisine.image} />
               <Card.Body>
-                <Card.Title>Card Title</Card.Title>
+                <Card.Title>{cuisine.name}</Card.Title>
                 <Card.Text>
-                  Some quick example text to build on the card title and make up the
-                  bulk of the card's content.
+                {cuisine.description}
                 </Card.Text>
                 
               </Card.Body>
