a79e1c8e6d95e6512a2b6154b14c2bdc309b21b7
CatherineKumar <catherine.a.kumar@gmail.com>
2024-04-23 02:37:29 -0500

    save work

--- assignments/SQL3 Assessment/SelectQueries1.sql
+++ assignments/SQL3 Assessment/SelectQueries1.sql
@@ -13,12 +13,28 @@ where name  = 'Cake'
 
 /* 3. Select all restaurants where restaurant names ends with 'Junction'. */
 
+Select *
+from restaurant
+where name like '%Junction'
+
 /* 4. Display details of all vegeterian fooditems. */
+Select *
+from fooditem
+where isVeg = 1
 
 /* 5. Retrieve all menu items with food items that have a price equal to or less than $10. */
+Select *
+from menu_items
+where fooditemPrice <= 1
 
 /* 6.Retrieve cartId having the maximum number of food items. */
-
+SELECT cartID
+FROM (
+  SELECT cartID, COUNT(DISTINCT fooditemId) as distinctCount
+  FROM cart_items
+  GROUP BY cartID
+) as subquery
+ORDER BY distinctCount DESC LIMIT 0, 1
 /* 7. Retrieve all the shipping details where emailId contains 'doe'. */
 
 /* 8. Retrieve the order with the highest price. */
