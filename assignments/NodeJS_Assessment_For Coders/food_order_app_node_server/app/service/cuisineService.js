66132c6c10058866a6acb8beda56952e4c93c435
CatherineKumar <catherine.a.kumar@gmail.com>
2024-05-13 23:09:18 -0500

    save work

--- assignments/NodeJS_Assessment_For Coders/food_order_app_node_server/app/service/cuisineService.js
+++ assignments/NodeJS_Assessment_For Coders/food_order_app_node_server/app/service/cuisineService.js
@@ -4,7 +4,15 @@ class Cuisines {
   constructor() {
     this.cuisines = JSON.parse(fs.readFileSync("./app/data/cuisines.json", "utf-8"));
   }
-  //COMPLETE TASK 1(b) HERE
+
+  getAllCuisines() {
+    return this.cuisines;
+  }
+
+  getCuisineById(id) {
+    const cuisine = this.cuisines.find((cuisine) => cuisine.id == id);
+    return cuisine;
+  }
   
 }
 
