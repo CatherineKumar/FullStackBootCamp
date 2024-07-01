b9dedcfe659bf98b8cd73fd3063237db2fc5d071
CatherineKumar <catherine.a.kumar@gmail.com>
2024-07-01 14:55:53 -0500

    save work

---
+++ examples/express_1/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get( '/', (req, res) => {
+    res.send('Hello World!')
+})
+
+const port = 4000;
+app.listen( (port, () => {
+    console.log(`App is listening on port ${port}`)
+
+}))
